805eae1fa0082a431e6e526f516f06c52e5ed56f
CodeMyThought <socialscmt@gmail.com>
2025-10-17 11:57:15 +0530

    Update helpers.js
    base url change

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -19,7 +19,8 @@ export function formatDate(date) {
  * - Automatically attaches token
  */
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://tms-git-main-cmts-projects-2ad7b35d.vercel.app/",
+  // baseURL: "http://localhost:3001/api",
   //  baseURL: "https://tms.codemythought.com/api",
   headers: {
     "Content-Type": "application/json",
