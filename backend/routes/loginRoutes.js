9ca8331e3087eec521b40b238f31a0aee745aa5f
shareedhar <shreedharniki@gmail.com>
2025-11-04 11:28:44 +0530

    login with name email phone  bckend change

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -1,15 +1,3 @@
-// const express = require("express");
-// const { login, forgotPassword, verifyOtp, resetPassword,changePassword, } = require("../controllers/loginController");
-// const { authenticate } = require("../middleware/auth");
-// const router = express.Router();
-
-// router.post("/", login);
-// router.post("/forgot-password", forgotPassword);
-// router.post("/verify-otp", verifyOtp);
-// router.post("/reset-password", resetPassword);
-// router.post("/change-password", authenticate, changePassword); // ✅ protected
-
-
 
 // module.exports = router;
 
